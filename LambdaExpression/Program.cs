b1f3991f844b1c3725ab32edd8223c24b167fa9d
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-08-22 10:51:44 +0530

    [jayesh]  create List

---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LambdaExpression
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //UC1 : Creating a list
+            PersonDetail personDetails = new PersonDetail();
+            personDetails.CreateList();
+        }
+    
+    }
+}
