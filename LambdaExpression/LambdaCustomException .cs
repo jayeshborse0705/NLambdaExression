d45ad4cdb46ea4f14c2d760223d029204f97f978
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-08-22 11:26:00 +0530

    [jayesh]  age below 60

---
+++ LambdaExpression/LambdaCustomException .cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    public class LambdaCustomException
+    {
+        Exceptiontype type;
+        public enum Exceptiontype
+        {
+            NO_SUCH_AGE
+        }
+        public LambdaCustomException(Exceptiontype type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
